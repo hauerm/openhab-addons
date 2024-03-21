f7721d1d749f2dac7871ccc40aa701d4d2807124
Michael Hauer <michael@hauers.eu>
2024-03-21 10:14:04 +0100

    add org.openhab.binding.burgenlandenergie addon

---
+++ bundles/org.openhab.binding.burgenlandenergie/src/main/java/org/openhab/binding/burgenlandenergie/internal/BurgenlandEnergieBindingConstants.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.burgenlandenergie.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.thing.ThingTypeUID;
+
+/**
+ * The {@link BurgenlandEnergieBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Michael Hauer - Initial contribution
+ */
+@NonNullByDefault
+public class BurgenlandEnergieBindingConstants {
+
+    private static final String BINDING_ID = "burgenlandenergie";
+
+    // List of all Thing Type UIDs
+
+    // The sales api thing (may add grid-api thing in the future)
+    public static final ThingTypeUID THING_SALES_API = new ThingTypeUID(BINDING_ID, "salesAPI");
+
+    // List of all Channel ids
+    public static final String CHANNEL_SALES_CURRENT_TARIFF_NAME = "currentTariffName";
+    public static final String CHANNEL_SALES_CURRENT_TARIFF_PRICE_KWH = "currentTariffPriceKWh";
+    public static final String CHANNEL_SALES_CURRENT_TARIFF_PRICE_FIXED = "currentTariffPriceFixed";
+}
---
+++ bundles/org.openhab.binding.burgenlandenergie/src/main/java/org/openhab/binding/burgenlandenergie/internal/config/SalesApiConfiguration.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.burgenlandenergie.internal.config;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link SalesApiConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Michael Hauer - Initial contribution
+ */
+@NonNullByDefault
+public class SalesApiConfiguration {
+
+    // The meine.burgenlandenergie.at username
+    public String username = "";
+
+    // The meine.burgenlandenergie.at password
+    public String password = "";
+}
